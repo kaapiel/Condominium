bb6acc28b88d397a5831b36663cb89a41ca5e470
Gabriel Aguido Fraga <gabriel_aguido@hotmail.com>
2023-01-02 15:12:19 -0500

    reduced 10x slower the sunlight

--- Assets/Resources/Scripts/LightingManager.cs
+++ Assets/Resources/Scripts/LightingManager.cs
@@ -20,7 +20,7 @@ public class LightingManager : MonoBehaviour
         if (Application.isPlaying)
         {
             //(Replace with a reference to the game time)
-            TimeOfDay += Time.deltaTime;
+            TimeOfDay += Time.deltaTime / 10;
             TimeOfDay %= 24; //Modulus to ensure always between 0-24
             UpdateLighting(TimeOfDay / 24f);
         }

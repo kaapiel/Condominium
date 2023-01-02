aad369137d7d54f8872c86f377430649a3e6a560
Gabriel Aguido Fraga <gabriel_aguido@hotmail.com>
2023-01-02 15:13:07 -0500

    unloading assetbundles when clicking on back button

--- Assets/Resources/Scripts/UICanvasControllerInput.cs
+++ Assets/Resources/Scripts/UICanvasControllerInput.cs
@@ -1,7 +1,8 @@
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace StarterAssets
 {
@@ -98,6 +99,54 @@ namespace StarterAssets
             }
         }
 
+        public void VirtualCustomInput()
+        {
+            FindIncludingInactive("ImportCanvas").active = true;
+            GameObject.Find("JoystickCanvas").active = false;
+        }
+
+        public void VirtualCustomCloseInput()
+        {
+            GameObject.Find("ImportCanvas").active = false;
+            FindIncludingInactive("JoystickCanvas").active = true;
+
+        }
+
+        public static GameObject FindIncludingInactive(string name)
+        {
+            Scene scene = SceneManager.GetActiveScene();
+            if (!scene.isLoaded)
+            {
+                //no scene loaded
+                return null;
+            }
+
+            var game_objects = new List<GameObject>();
+            scene.GetRootGameObjects(game_objects);
+
+            foreach (GameObject obj in game_objects)
+            {
+                if (obj.transform.name == name) return obj;
+
+                GameObject found = FindInChildrenIncludingInactive(obj, name);
+                if (found) return found;
+            }
+
+            return null;
+        }
+
+        private static GameObject FindInChildrenIncludingInactive(GameObject go, string name)
+        {
+
+            for (int i = 0; i < go.transform.childCount; i++)
+            {
+                if (go.transform.GetChild(i).gameObject.name == name) return go.transform.GetChild(i).gameObject;
+                GameObject found = FindInChildrenIncludingInactive(go.transform.GetChild(i).gameObject, name);
+                if (found != null) return found;
+            }
+
+            return null;  //couldn't find crap
+        }
     }
 
 }
